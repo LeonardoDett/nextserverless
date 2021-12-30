3264f53cd807007c7505c337d48da8a3bb22cbb9
Leonardo Dettmann Krüger <leokruger2012@gmail.com>
2021-12-30 09:55:04 -0300

    deployed

---
+++ .serverless_nextjs/api-lambda/pages/api/hello.js
@@ -0,0 +1,179 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 453;
+exports.ids = [453];
+exports.modules = {
+
+/***/ 4300:
+/***/ ((module) => {
+
+module.exports = require("buffer");
+
+/***/ }),
+
+/***/ 6113:
+/***/ ((module) => {
+
+module.exports = require("crypto");
+
+/***/ }),
+
+/***/ 2361:
+/***/ ((module) => {
+
+module.exports = require("events");
+
+/***/ }),
+
+/***/ 7147:
+/***/ ((module) => {
+
+module.exports = require("fs");
+
+/***/ }),
+
+/***/ 3685:
+/***/ ((module) => {
+
+module.exports = require("http");
+
+/***/ }),
+
+/***/ 5687:
+/***/ ((module) => {
+
+module.exports = require("https");
+
+/***/ }),
+
+/***/ 1017:
+/***/ ((module) => {
+
+module.exports = require("path");
+
+/***/ }),
+
+/***/ 3477:
+/***/ ((module) => {
+
+module.exports = require("querystring");
+
+/***/ }),
+
+/***/ 2781:
+/***/ ((module) => {
+
+module.exports = require("stream");
+
+/***/ }),
+
+/***/ 1576:
+/***/ ((module) => {
+
+module.exports = require("string_decoder");
+
+/***/ }),
+
+/***/ 7310:
+/***/ ((module) => {
+
+module.exports = require("url");
+
+/***/ }),
+
+/***/ 3837:
+/***/ ((module) => {
+
+module.exports = require("util");
+
+/***/ }),
+
+/***/ 9796:
+/***/ ((module) => {
+
+module.exports = require("zlib");
+
+/***/ }),
+
+/***/ 7559:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ next_serverless_loaderpage_2Fapi_2Fhello_absolutePagePath_private_next_pages_2Fapi_2Fhello_ts_absoluteAppPath_private_next_pages_2F_app_tsx_absoluteDocumentPath_next_2Fdist_2Fpages_2F_document_absoluteErrorPath_next_2Fdist_2Fpages_2F_error_absolute404Path_distDir_private_dot_next_buildId_WZpPXGUdTyqWocrDawVFc_assetPrefix_generateEtags_true_poweredByHeader_true_canonicalBase_basePath_runtimeConfig_previewProps_7B_22previewModeId_22_3A_221d81659159aa5c394e76181215b0df2b_22_2C_22previewModeSigningKey_22_3A_22e82b597a0511f5208039a6ab94f3e918a37e970cd3f44d53e7fdf46e49154d9f_22_2C_22previewModeEncryptionKey_22_3A_226f244766273985044175fbca27800ce6f8f66889c54ff778e9570bcbea70288d_22_7D_loadedEnvFiles_W10_3D_i18n_)
+});
+
+// EXTERNAL MODULE: ./node_modules/next/dist/server/node-polyfill-fetch.js
+var node_polyfill_fetch = __webpack_require__(7157);
+;// CONCATENATED MODULE: ./.next/routes-manifest.json
+const routes_manifest_namespaceObject = {"Dg":[]};
+// EXTERNAL MODULE: ./node_modules/next/dist/build/webpack/loaders/next-serverless-loader/api-handler.js
+var api_handler = __webpack_require__(6249);
+;// CONCATENATED MODULE: ./node_modules/next/dist/build/webpack/loaders/next-serverless-loader/index.js?page=%2Fapi%2Fhello&absolutePagePath=private-next-pages%2Fapi%2Fhello.ts&absoluteAppPath=private-next-pages%2F_app.tsx&absoluteDocumentPath=next%2Fdist%2Fpages%2F_document&absoluteErrorPath=next%2Fdist%2Fpages%2F_error&absolute404Path=&distDir=private-dot-next&buildId=WZpPXGUdTyqWocrDawVFc&assetPrefix=&generateEtags=true&poweredByHeader=true&canonicalBase=&basePath=&runtimeConfig=&previewProps=%7B%22previewModeId%22%3A%221d81659159aa5c394e76181215b0df2b%22%2C%22previewModeSigningKey%22%3A%22e82b597a0511f5208039a6ab94f3e918a37e970cd3f44d53e7fdf46e49154d9f%22%2C%22previewModeEncryptionKey%22%3A%226f244766273985044175fbca27800ce6f8f66889c54ff778e9570bcbea70288d%22%7D&loadedEnvFiles=W10%3D&i18n=!
+
+        
+      const { processEnv } = __webpack_require__(9936)
+      processEnv([])
+    
+        
+        const runtimeConfig = {}
+        ;
+        
+
+        
+
+        const combinedRewrites = Array.isArray(routes_manifest_namespaceObject.Dg)
+          ? routes_manifest_namespaceObject.Dg
+          : []
+
+        if (!Array.isArray(routes_manifest_namespaceObject.Dg)) {
+          combinedRewrites.push(...routes_manifest_namespaceObject.Dg.beforeFiles)
+          combinedRewrites.push(...routes_manifest_namespaceObject.Dg.afterFiles)
+          combinedRewrites.push(...routes_manifest_namespaceObject.Dg.fallback)
+        }
+
+        const apiHandler = (0,api_handler/* getApiHandler */.Y)({
+          pageModule: __webpack_require__(8505),
+          rewrites: combinedRewrites,
+          i18n: undefined,
+          page: "/api/hello",
+          basePath: "",
+          pageIsDynamic: false,
+          encodedPreviewProps: {previewModeId:"1d81659159aa5c394e76181215b0df2b",previewModeSigningKey:"e82b597a0511f5208039a6ab94f3e918a37e970cd3f44d53e7fdf46e49154d9f",previewModeEncryptionKey:"6f244766273985044175fbca27800ce6f8f66889c54ff778e9570bcbea70288d"}
+        })
+        /* harmony default export */ const next_serverless_loaderpage_2Fapi_2Fhello_absolutePagePath_private_next_pages_2Fapi_2Fhello_ts_absoluteAppPath_private_next_pages_2F_app_tsx_absoluteDocumentPath_next_2Fdist_2Fpages_2F_document_absoluteErrorPath_next_2Fdist_2Fpages_2F_error_absolute404Path_distDir_private_dot_next_buildId_WZpPXGUdTyqWocrDawVFc_assetPrefix_generateEtags_true_poweredByHeader_true_canonicalBase_basePath_runtimeConfig_previewProps_7B_22previewModeId_22_3A_221d81659159aa5c394e76181215b0df2b_22_2C_22previewModeSigningKey_22_3A_22e82b597a0511f5208039a6ab94f3e918a37e970cd3f44d53e7fdf46e49154d9f_22_2C_22previewModeEncryptionKey_22_3A_226f244766273985044175fbca27800ce6f8f66889c54ff778e9570bcbea70288d_22_7D_loadedEnvFiles_W10_3D_i18n_ = (apiHandler);
+      
+
+/***/ }),
+
+/***/ 8505:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ handler)
+/* harmony export */ });
+function handler(req, res) {
+    res.status(200).json({
+        name: 'John Doe'
+    });
+};
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [458], () => (__webpack_exec__(7559)));
+module.exports = __webpack_exports__;
+
+})();
---
+++ .serverless_nextjs/default-lambda/chunks/640.js
@@ -0,0 +1,48 @@
+"use strict";
+exports.id = 640;
+exports.ids = [640];
+exports.modules = {
+
+/***/ 5656:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(8521);
+
+
+function MyApp({ Component , pageProps  }) {
+    return(/*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
+        ...pageProps
+    }));
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MyApp);
+
+
+/***/ }),
+
+/***/ 7020:
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"polyfillFiles":["static/chunks/polyfills-5cd94c89d3acac5f.js"],"devFiles":[],"ampDevFiles":[],"lowPriorityFiles":["static/WZpPXGUdTyqWocrDawVFc/_buildManifest.js","static/WZpPXGUdTyqWocrDawVFc/_ssgManifest.js","static/WZpPXGUdTyqWocrDawVFc/_middlewareManifest.js"],"pages":{"/":["static/chunks/webpack-514908bffb652963.js","static/chunks/framework-6e4ba497ae0c8a3f.js","static/chunks/main-7e73d61bce33e887.js","static/css/b282b959608f58d4.css","static/chunks/pages/index-7245e202d26de608.js"],"/_app":["static/chunks/webpack-514908bffb652963.js","static/chunks/framework-6e4ba497ae0c8a3f.js","static/chunks/main-7e73d61bce33e887.js","static/css/2974ebc8daa97b04.css","static/chunks/pages/_app-ca6aae25bc99a05f.js"],"/_error":["static/chunks/webpack-514908bffb652963.js","static/chunks/framework-6e4ba497ae0c8a3f.js","static/chunks/main-7e73d61bce33e887.js","static/chunks/pages/_error-2280fa386d040b66.js"]},"ampFirstPages":[]}');
+
+/***/ }),
+
+/***/ 3978:
+/***/ ((module) => {
+
+module.exports = {};
+
+/***/ }),
+
+/***/ 9450:
+/***/ ((module) => {
+
+module.exports = {"Dg":[]};
+
+/***/ })
+
+};
+;
